14e6a418a46baeeefab5119df2870b6e2fe4dcd6
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2023-07-27 09:43:05 -0300

    feat: add template

--- quarzza-api/src/main/java/quarzza/GreetingResource.java
+++ quarzza-api/src/main/java/quarzza/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello From Quarzzar Pizzaria!";
+        return "Hello From Quarzzar Pizzaria! :pizza:";
     }
 }
